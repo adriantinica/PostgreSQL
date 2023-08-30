e269a806737036141455f3eb8239c0b586776f86
Tinica Adrian <118120924+adriantinica@users.noreply.github.com>
2023-08-30 11:42:42 +0300

    Update App.java
    added C and D from CRUD in this DB;

--- e-sop-DB/e-shop/src/main/java/student/example/App.java
+++ e-sop-DB/e-shop/src/main/java/student/example/App.java
@@ -20,23 +20,14 @@ public class App
 
         Statement st = conn.createStatement();
         ResultSet rs = st.executeQuery("SELECT * FROM currencies ");
+       
+        
 
-        // ITERATING lines and print;
-       // rs.next();
-        //System.out.println(rs.getInt(1));
-        //System.out.println(rs.getString(2));
-        //System.out.println(rs.getString(3));
-        //System.out.println(rs.getDouble(4));
-        //rs.next();
-        //System.out.println(rs.getInt(1));
-       // System.out.println(rs.getString(2));
-        //System.out.println(rs.getString(3));
-        //System.out.println(rs.getDouble(4));
         
         //HW1: Let's add new fields in our table (currencies):
 
-        String sql = "INSERT INTO currencies (id, name, char_code, rate) VALUES (?, ?, ?, ?)";
-
+        String sql = "INSERT INTO currencies (id, name, char_code, rate) VALUES (?, ?, ?, ?)" +
+                     "ON CONFLICT (id) DO NOTHING";
           
             // Create a PreparedStatement and set the values for the placeholders
             PreparedStatement preparedStatement = conn.prepareStatement(sql);
@@ -48,14 +39,29 @@ public class App
             preparedStatement.setString(2, currencyName);
             preparedStatement.setString(3, currencyCode);
             preparedStatement.setDouble(4, exchangeRate);
-
+            
             // Execute the insert statement
             preparedStatement.executeUpdate();
+
+            PreparedStatement preparedStatement1 = conn.prepareStatement(sql);
+            int id1 = 643;
+            currencyName = "Russian Ruble";
+            currencyCode = "RUB";
+            exchangeRate = 0.1865;
+            preparedStatement1.setInt(1, id1);
+            preparedStatement1.setString(2, currencyName);
+            preparedStatement1.setString(3, currencyCode);
+            preparedStatement1.setDouble(4, exchangeRate);
+             
+
+            // Execute the insert statement
+            preparedStatement1.executeUpdate();
+           
             
+
 
         
         rs = st.executeQuery("SELECT * FROM currencies ");
-
         // ITERATING lines and print;
         while (rs.next()) {
         System.out.println(rs.getInt(1));
@@ -64,8 +70,39 @@ public class App
         System.out.println(rs.getDouble(4));
         }
 
+        String dropPrimaryKey = "ALTER TABLE currencies DROP CONSTRAINT IF EXISTS currencies_pkey";
+        st.executeUpdate(dropPrimaryKey);
+
+        // Then, add the new primary key
         String setPrimaryKey = "ALTER TABLE currencies ADD PRIMARY KEY (id)";
         st.executeUpdate(setPrimaryKey);
+
+
+        
+
+        String alterDelete ="DELETE FROM currencies WHERE id = 643 ";
+        st.executeUpdate(alterDelete);
+        System.out.println("after DELETE ALTERATION !!!");
+
+        
+        rs = st.executeQuery("SELECT * FROM currencies ");
+        while (rs.next()) {
+        System.out.println(rs.getInt(1));
+        System.out.println(rs.getString(2));
+        System.out.println(rs.getString(3));
+        System.out.println(rs.getDouble(4));
+        }
+        
+        preparedStatement1.close();
+        preparedStatement.close();
+        st.close();
+        conn.close();
+
+        
+       
+    }
+}
+
         
 
         
